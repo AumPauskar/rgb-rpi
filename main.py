fa76986fa8773b056cfd65e659af498e0277133b
Aum Pauskar <aumpauksar111@gmail.com>
2023-12-31 18:29:55 +0530

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ file_path = "config.json"
 # Open the JSON file and load its contents
 with open(file_path) as file:
 	data = json.load(file)
-	print(data["gpio-red"])
-	print(data["gpio-green"])
-	print(data["gpio-blue"])
+	gpio_red = data["gpio-red"]
+	gpio_green = data["gpio-green"]
+	gpio_blue = data["gpio-blue"]
 
